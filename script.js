e97203e40ee400725138e0d52966a97fd52ffadf
Shivangam Soni <shivangamsoni99@gmail.com>
2021-09-03 17:24:11 +0530

    Background Color Changed

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+/**
+ * @author Shivangam Soni <shiavngamsoni99@gmail.com>
+ * @see <a href="https://github.com/ShivangamSoni">GitHub</a>
+ */
 const projects = document.querySelectorAll(".project");
 let num = 1;
 projects.forEach(project => project.style.backgroundImage = `url("./images/${num++}.png")`);
